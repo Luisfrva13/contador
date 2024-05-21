cf5057b9ec6d40f4e538cfdd89a038324f80452a
Luis_Fernando <luisfernandoveras13@gmail.com>
2024-05-21 19:55:44 -0300

    versão 1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+# USUÁRIO INFORMA NÚMERO
+contador = int(input('Informe um número inteiro:'))
+
+# CONTAR DO VALOR INFORMADO ATÉ 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
+    
