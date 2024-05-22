657af4358de38b77e6fc90d1a5767389ab3911e2
Luis_Fernando <luisfernandoveras13@gmail.com>
2024-05-21 21:11:59 -0300

    versão 1.1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
+import time
+
 # USUÁRIO INFORMA NÚMERO
 contador = int(input('Informe um número inteiro:'))
+os.system('cls')
 
 # CONTAR DO VALOR INFORMADO ATÉ 0
 while contador >= 0:
-    print(contador)
+    print(f'Contagem regressiva: {contador}...')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
-    
+
+print('BOOOM!!!')
+
+
+
